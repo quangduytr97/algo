57a6e6d1268061a7b975c6eb9f115e7ca624c817
Trần Quang Duy <duy.tranquang@teragroup.io>
2023-12-30 20:10:34 +0700

    RedistributeCharactersToMakeAllStringsEqual

---
+++ Leetcode/RedistributeCharactersToMakeAllStringsEqual/src/Solution.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public boolean makeEqual(String[] words) {
+        Map<Character, Integer> counts = new HashMap<>();
+
+        for (String word : words) {
+            for (char c : word.toCharArray()) {
+                counts.put(c, counts.getOrDefault(c, 0) + 1);
+            }
+        }
+
+        int n = words.length;
+        for (Map.Entry<Character, Integer> entry : counts.entrySet()) {
+            if (entry.getValue() % n != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
