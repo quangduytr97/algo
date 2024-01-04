ef96068e1ace98d40fd47f9b56f20bb63cb5dc2e
Trần Quang Duy <duy.tranquang@teragroup.io>
2024-01-04 19:19:41 +0700

    MinimumNumberOfOperationsToMakeArrayEmpty

---
+++ Leetcode/MinimumNumberOfOperationsToMakeArrayEmpty/src/Solution.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public int minOperations(int[] nums) {
+        Map<Integer, Integer> mp = new HashMap<>();
+
+        for (int i : nums) {
+            mp.put(i, mp.getOrDefault(i, 0) + 1);
+        }
+
+        int ans = 0;
+
+        for (int i : mp.values()) {
+            if (i == 1) {
+                return -1;
+            }
+
+            ans += (i + 2) / 3;
+        }
+
+        return ans;
+    }
+}
