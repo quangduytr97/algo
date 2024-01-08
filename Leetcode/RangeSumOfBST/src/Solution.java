6993376c1630b0f86aee79c9ec8e21dfc53368e7
Trần Quang Duy <duy.tranquang@teragroup.io>
2024-01-08 22:51:53 +0700

    RangeSumOfBTB

---
+++ Leetcode/RangeSumOfBST/src/Solution.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+
+class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+  }
+class Solution {
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        int sum = 0;
+        Stack<TreeNode> stack = new Stack<>();
+
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+
+            root = stack.pop();
+
+            if (root.val >= L && root.val <= R) {
+                sum += root.val;
+            }
+
+            root = root.right;
+        }
+
+        return sum;
+    }
+}
