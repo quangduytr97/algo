b5535f61d2c3e2de092d25ef9369dc05f23d02fa
Trần Quang Duy <duy.tranquang@teragroup.io>
2024-02-20 07:55:57 +0700

    MissingNumber

---
+++ Leetcode/MissingNumber/src/Solution.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int c = 0;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == c) {
+                c++;
+            } else return c;
+        }
+        return c;
+    }
+}
