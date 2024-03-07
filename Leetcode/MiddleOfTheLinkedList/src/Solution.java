69a69f3e3b33b718557473c3857de49f9f177b1f
Trần Quang Duy <duy.tranquang@teragroup.io>
2024-03-07 08:01:31 +0700

    MiddleOfTheLinkedList

---
+++ Leetcode/MiddleOfTheLinkedList/src/Solution.java
@@ -0,0 +1,23 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        if (head == null) return null;
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        if (fast.next == null) return slow;
+        else return slow.next;
+    }
+}
