cc2487aafe973ba4e13ae796dd60732a22f9d95a
Trần Quang Duy <duy.tranquang@teragroup.io>
2024-03-12 21:27:51 +0700

    RemoveZeroSumConsecutiveNodesFromLinkedList

---
+++ Leetcode/RemoveZeroSumConsecutiveNodesFromLinkedList/src/Solution.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+
+class Solution {
+    public ListNode removeZeroSumSublists(ListNode head) {
+
+        ListNode dummy = new ListNode(0, head);
+        ListNode curr = dummy;
+        int prefixSum = 0;
+
+        Map<Integer, ListNode> map = new HashMap<>();
+        map.put(0, dummy);
+
+        while(curr != null) {
+            prefixSum += curr.val;
+            map.put(prefixSum, curr);
+            curr = curr.next;
+        }
+
+        prefixSum = 0;
+        curr = dummy;
+
+        while(curr != null) {
+            prefixSum += curr.val;
+            curr.next = map.get(prefixSum).next;
+            curr = curr.next;
+        }
+        return dummy.next;
+    }
+}
